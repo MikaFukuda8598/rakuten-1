9462b1d1bada8d4fa1e62ea59123ca66e22a80b5
MikaFukuda8598 <mikg4505@gmail.com>
2021-03-28 09:57:16 +0000

    並び替え機能の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
    before_action :set_product, only: [:show, :create, :edit, :update, :destroy, :favorite]
-   PER = 15
   
   def index
-    @products = Product.page(params[:page]).per(PER)
+   if sort_params.present?
+    @category = Category.request_category(sort_params[:sort_category])
+    @products = Product.sort_products(sort_params, params[:page])
+   elsif params[:category].present?
+    @category = Category.request_category(params[:category])
+    @products = Product.category_products(@category, params[:page])
+   else
+    @products = Product.display_list(params[:page])
+   end
+   
+    @major_category_names = Category.major_categories
+    @categories = Category.all
+    @sort_list = Product.sort_list
   end
 
   def show
@@ -43,11 +54,15 @@ class ProductsController < ApplicationController
   
   private
   
-   def set_product
+  def set_product
       @product = Product.find(params[:id])
-   end
+  end
      
-   def product_params
+  def product_params
     params.require(:product).permit(:name, :description, :price, :category_id)
-   end
+  end
+
+  def sort_params
+     params.permit(:sort, :sort_category)
+  end
 end
